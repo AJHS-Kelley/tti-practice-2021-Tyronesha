834465f1097361242db8cb754e191bad1a87ddc0
Tyronesha <90337062+Tyronesha@users.noreply.github.com>
2021-11-30 10:04:13 -0500

    11/30/21 v.01

--- .github/Try_except.py
+++ .github/Try_except.py
@@ -8,3 +8,13 @@ try:
 except:
     print("Oh no, something is burning!\n")
 
+try:
+    print(z)
+except NameError:
+    print("That variable is not defined, let's define it.\n")
+    z=25
+except:
+    print("Oh no, something is burning!\n")
+finally:
+    print("The try/catch is finished.")
+print(z)
